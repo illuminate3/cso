8971bd3422466fae02be4fb68648d33ac893863b
kilowat <kadantsev.sergey@gmail.com>
2015-05-24 23:16:37 +0400

    working

--- app/Admin/Personal.php
+++ app/Admin/Personal.php
@@ -8,20 +8,26 @@ Admin::model('App\Model\Personal')->title('')->display(function ()
 
 	]);
 	$display->columns([
-		Column::string('name')->label('Name'),
-		Column::string('eduction')->label('Eduction'),
-		Column::string('exp')->label('Exp'),
-		Column::string('course')->label('Course'),
+		Column::string('soname')->label('Фамилия'),
+		Column::string('name')->label('Имя'),
+		Column::string('second_name')->label('Отчество'),
+		Column::string('eduction')->label('Образование'),
+		Column::string('post')->label('Должность'),
+		Column::string('exp')->label('Дата приема'),
+
 	]);
 	return $display;
 })->createAndEdit(function ()
 {
 	$form = AdminForm::form();
 	$form->items([
-		FormItem::text('name', 'Фио'),
+		FormItem::text('soname', 'Фамилия'),
+		FormItem::text('name', 'Имя'),
+		FormItem::text('second_name', 'Отчество'),
 		FormItem::text('eduction', 'Образование'),
-		FormItem::text('exp', 'Опыт'),
-		FormItem::text('course', 'Доп образование, курсы'),
+		FormItem::text('post', 'Должность'),
+		FormItem::date('exp', 'Дата приема'),
+
 	]);
 	return $form;
 });
